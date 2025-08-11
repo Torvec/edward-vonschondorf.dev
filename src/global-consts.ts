980f10ebe6bbdde59f35edc6095539137ac5dd35
vonedward <me@edward-vonschondorf.dev>
2025-08-10 22:05:08 -0700

    Fixed a minor issue where external link in the main nav bar were not setup to open in a new tab/window

--- src/global-consts.ts
+++ src/global-consts.ts
@@ -30,31 +30,37 @@ export const socialLinks = [
   {
     name: "Email",
     href: MY_EMAIL,
+    external: false,
     Icon: Gmail,
   },
   {
     name: "GitHub",
     href: "https://github.com/Torvec",
+    external: true,
     Icon: GitHub,
   },
   {
     name: "LinkedIn",
     href: "https://www.linkedin.com/in/edward-vonschondorf/",
+    external: true,
     Icon: LinkedIn,
   },
   {
     name: "Bluesky",
     href: "https://bsky.app/profile/edward-vonschondorf.dev",
+    external: true,
     Icon: Bluesky,
   },
   {
     name: "Mastodon",
     href: "https://mastodon.social/@edvonschondorf",
+    external: true,
     Icon: Mastodon,
   },
   {
     name: "YouTube",
     href: "https://www.youtube.com/@edward-vonschondorf-dev",
+    external: true,
     Icon: YouTube,
   },
 ];
